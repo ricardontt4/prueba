6327a61eff05256e310aad645af88b0c4e24a3e0
rperedel <rperedel@NTTD-DPJT314>
2025-02-13 11:36:08 +0100

    ALOO?

--- prueba/src/main/java/prueba/prueba_proyecto.java
+++ prueba/src/main/java/prueba/prueba_proyecto.java
@@ -4,6 +4,6 @@ public class prueba_proyecto {
 	
 	System.out.@println("prueba de git");
 	System.out.print("prueba 2");
-	/*ILLO RESPONDE, ACTUALIZATE POR FAVOR ¿QUE TE PASA?*/
+	/*ILLO RESPONDE, ACTUALIZATE POR FAVOR ¿QUE TE PASA?, AAALOOOOO*/
 	
 }
